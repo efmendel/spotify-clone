0f784190e0706c20befdd8110bfcc3a9eb0e391c
efmendel <edwardfmendel@gmail.com>
2025-08-03 23:39:34 -0400

    createsong controller

--- backend/src/controller/admin.controller.js
+++ backend/src/controller/admin.controller.js
@@ -1,3 +1,20 @@
-export const createSong = (req, res) => {
-    res.send("Admin route with GET method");
+import { Song } from "../models/song.model";
+
+export const createSong = async (req, res) => {
+    try {
+        if (!req.files || !req.files.audioFile || !req.files.imageFile) {
+            return res.status(400).json({message: "Please uplaod all files"})
+        }
+
+        const {title, artist, albumId, duration} = req.body;
+        const audioFile = req.files.audioFile;
+        const imageFile = req.files.imageFile;
+
+        const song = new Song({
+            
+        });
+
+    } catch (error) {
+        
+    }
 }
