6a10fbfe172cdb8aae42b10c2b6b4156420090f9
efmendel <edwardfmendel@gmail.com>
2025-08-07 23:56:37 -0400

    update to createsong function
    fix name of one of models and added possibility for update and error handling

--- backend/src/controller/admin.controller.js
+++ backend/src/controller/admin.controller.js
@@ -1,20 +1,36 @@
-import { Song } from "../models/song.model";
+import { Song } from "../models/song.model.js";
+import { Album } from "../models/album.model.js";
 
 export const createSong = async (req, res) => {
-    try {
-        if (!req.files || !req.files.audioFile || !req.files.imageFile) {
-            return res.status(400).json({message: "Please uplaod all files"})
-        }
-
-        const {title, artist, albumId, duration} = req.body;
-        const audioFile = req.files.audioFile;
-        const imageFile = req.files.imageFile;
-
-        const song = new Song({
-            
-        });
-
-    } catch (error) {
-        
+  try {
+    if (!req.files || !req.files.audioFile || !req.files.imageFile) {
+      return res.status(400).json({ message: "Please uplaod all files" });
     }
-}
+
+    const { title, artist, albumId, duration } = req.body;
+    const audioFile = req.files.audioFile;
+    const imageFile = req.files.imageFile;
+
+    const song = new Song({
+      title,
+      artist,
+      audioURL,
+      imageURL,
+      duration,
+      albumId: albumId || null,
+    });
+
+    await song.save();
+
+    // if the song belongs to an album, then it updates the album's song array
+    if (albumId) {
+      await Album.findByIdAndUpdate(albumId, {
+        $push: { songs: song._id },
+      });
+    }
+    res.status(201).json(song);
+  } catch (error) {
+    console.log("Error in createsong", error);
+    res.status(500).json({message: "Internal server error", error})
+  }
+};
