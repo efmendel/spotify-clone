597b2c5c5fb055ea7ebddf75b945a2db113b28ad
efmendel <edwardfmendel@gmail.com>
2025-08-23 23:10:04 -0400

    Finished album routing, song routing, and in the middle of stat routing

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-import {clerkMiddleware} from '@clerk/express';
-import {fileUpload} from 'express-fileupload';
+import { clerkMiddleware } from "@clerk/express";
+import fileUpload from "express-fileupload";
 import path from "path";
 
 import { connectDB } from "./lib/db.js";
@@ -17,34 +17,43 @@ dotenv.config();
 const app = express();
 const __dirname = path.resolve();
 const PORT = process.env.PORT;
+console.log(PORT)
 
 // to parse req.body, utility middleware (universal and is applied to every single incoming request)
-app.use(express.json())
+app.use(express.json());
 app.use(clerkMiddleware()); // adds auth to req object => req.auth
-app.use(fileUpload({
+app.use(
+  fileUpload({
     useTempFiles: true,
     tempFileDir: path.join(__dirname, "tmp"),
     createParentPath: true,
     limits: {
-        fileSize: 10 * 1024 * 1024 // 10 mb file size
+      fileSize: 10 * 1024 * 1024, // 10 mb file size
     },
-    
-}))
+  })
+);
 
 // router middleware that checks every request to see if the path is matching, and if so runs the function
-app.use("/api/users", userRoutes)
-app.use("/api/auth", authRoutes)
-app.use("/api/admin", adminRoutes)
-app.use("/api/songs", songRoutes)
-app.use("/api/albums", albumRoutes)
-app.use("/api/stats", statRoutes)
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/songs", songRoutes);
+app.use("/api/albums", albumRoutes);
+app.use("/api/stats", statRoutes);
 
 // error handler
 app.use((err, req, res, next) => {
-    res.status(500).json({message: process.env.NODE_ENV === "production" ? "Internal sever error": err.message})
-})
+  res
+    .status(500)
+    .json({
+      message:
+        process.env.NODE_ENV === "production"
+          ? "Internal sever error"
+          : err.message,
+    });
+});
 
-app.listen(PORT, ()=> {
-    console.log("server is running on port " + PORT);
-    connectDB();
-})
+app.listen(PORT, () => {
+  console.log("server is running on port " + PORT);
+  connectDB();
+});
--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,7 +1,7 @@
 import { clerkClient } from "@clerk/express";
 
 export const protectRoute = async (req, res, next) => {
-    if(!req.auth.userID) {
+    if(!req.auth.userId) {
         return res.status(401).json({message: "Unauthorized - You must be logged in"})
     }
 
@@ -10,7 +10,7 @@ export const protectRoute = async (req, res, next) => {
 
 export const requireAdmin = async (req, res, next) => {
     try {
-        const currentUser = await clerkClient.users.getUser(req.auth.userID);
+        const currentUser = await clerkClient.users.getUser(req.auth.userId);
         const isAdmin = process.env.ADMIN_EMAIL === currentUser.primaryEmailAddress.emailAddress
 
         if (!isAdmin) {
