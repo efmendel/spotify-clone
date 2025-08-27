677f792af0d5db90cf35ca5d0be5b0800c67025a
efmendel <edwardfmendel@gmail.com>
2025-08-26 22:06:25 -0400

    frontend layout skeleton

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,14 @@
-import {
-  SignedOut,
-  SignInButton,
-  SignedIn,
-  UserButton,
-  AuthenticateWithRedirectCallback,
-} from "@clerk/clerk-react";
-import { Button } from "./components/ui/button";
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
-import { axiosInstance } from "./lib/axios";
+import MainLayout from "./layout/MainLayout";
+import ChatPage from "./pages/chat/ChatPage";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
         <Route
           path="/sso-callback"
           element={
@@ -25,6 +18,10 @@ function App() {
           }
         />
         <Route path="/auth-callback" element={<AuthCallbackPage />} />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/chat" element={<ChatPage />} />
+        </Route>
       </Routes>
     </>
   );
