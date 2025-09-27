930aa09cc324aea2305471ee45b6ee8a39a09b22
efmendel <edwardfmendel@gmail.com>
2025-09-27 16:32:22 -0700

    Update axios.ts

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://spotify-clone-production-c51c.up.railway.app/api",
-})
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api" // Local development
+      : "https://spotify-clone-production-c51c.up.railway.app/api", // Production
+});
